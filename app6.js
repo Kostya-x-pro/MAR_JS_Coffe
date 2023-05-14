7638e4534fda9d7c869b585a2f7cd0f0e0c337f7
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-05-14 17:57:49 +0300

    added basket

--- app6.js
+++ app6.js
@@ -4,6 +4,15 @@ const shopBody = document.querySelector('.shop__body');
 const dropdownLinks = document.querySelectorAll('.dropdown__link');
 const formInput = document.querySelector('.form__input');
 const formBtn = document.querySelector('.form__btn');
+const modal = document.querySelector('.modal');
+const modalBody = modal.querySelector('.modal__body');
+const basket = document.querySelector('.basket');
+const buyList = modalBody.querySelector('tbody');
+const btnClose = document.createElement('div');
+
+
+btnClose.classList.add('modal-close');
+modalBody.prepend(btnClose);
 
 dropdown.addEventListener('click', ()=> {
   dropdownList.classList.toggle('active');
@@ -17,201 +26,217 @@ dropdown.addEventListener('click', ()=> {
 });
 
 dropdownLinks.forEach(link => {
- 
-  link.addEventListener('click', ()=> {
-    shopBody.innerHTML = '';
 
-    return (link.innerHTML === 'ALL PRODUCTS') ?
-            cards.forEach((item, i) => {
-              showProduct(i);
-            })                                 : 
-  
-            cards.forEach((item, i)=> {
-              if (item.data === link.innerHTML) {
-                showProduct(i);
-              }
-            });
+  link.addEventListener('click', ()=> {
+    const newArr = [];
+    // console.log(link.innerHTML); // тип кофе
+    let typeCoffe = link.innerHTML;
+    cards.forEach(el => {
+      if (el.data === typeCoffe) {
+        newArr.push(el);
+      } else if (typeCoffe === 'ALL PRODUCTS') {
+        newArr.push(el);
+      }
+    });
+    paintCards(newArr);
+
   });
 });
 
+const imagesObj = {
+  ETHIOPIA: 'shop__image_bg1',
+  KENYA: 'shop__image_bg2',
+  QUATEMALA: 'shop__image_bg3',
+  COLUMBIA: 'shop__image_bg4',
+};
+
 // Функция которая отрисовывает блок shop__body
-function showProduct(i) {
-  const shopItem = document.createElement('div');
-  shopItem.classList.add('shop__item');
-  shopBody.append(shopItem);
-  
-  const shopImg = document.createElement('div');
-  shopImg.classList.add('shop__image');
-  shopImg.style.backgroundImage = `url(${cards[i].img})`;
-  shopItem.append(shopImg);
+function paintCards(arr) {
+  shopBody.innerHTML = '';
+  arr.forEach(elem => {
+      const cardItem = document.createElement('div');
+      cardItem.classList.add('shop__item');
+      const card = document.createElement('div');
+      card.innerHTML = `
+      <div class="shop__image ${imagesObj[elem.data]}">
+      <a class="shop__link" data-id='${elem.id}' href="##">В корзину</a></div>
+      <h3 class="shop__image_title">${elem.name}</h3>
+      <p>${elem.price}</p>
+      `;
+      cardItem.append(card);
+      shopBody.append(cardItem);
 
-  const shopTitle = document.createElement('h3');
-  shopTitle.classList.add('shop__image_title');
-  shopTitle.innerHTML = cards[i].name; // [i] номер объекта по порядку
-  shopItem.append(shopTitle);
-
-  const price = document.createElement('p');
-  price.innerHTML = cards[i].price; // [i] номер объекта по порядку
-  shopItem.append(price);
+  });
  }
 
 // Функция искать товар.
  const searchCards = (event) => {
-    event.preventDefault();
-    shopBody.innerHTML = '';
-    let inputValue = formInput.value;
+   event.preventDefault();
+   let inputValue = formInput.value;
 
-    const findCards = cards.filter(el => el.name.toLowerCase().includes(inputValue.toLowerCase().trim()));
+   const findCards = cards.filter(el => el.name.toLowerCase().includes(inputValue.toLowerCase().trim()));
+   
+   if (findCards.length !== 0) {
+     paintCards(findCards);
+  }
+};
 
-    findCards.forEach((el, i) => {
-      if (el.name.toLowerCase().includes(inputValue) && inputValue !== 0) {
-        printCards(i);
-      }
-    });
+// Скрытие модального окна
+modal.addEventListener('click', (e)=> {
+  if (e.target === btnClose || e.target === modal) {
+    modal.classList.remove('modal-active');
+  }
+});
 
+// Добавление класса при клике на корзину
+basket.addEventListener('click', ()=> {
+  modal.classList.toggle('modal-active');
 
-    function printCards(i) {
-      const shopItem = document.createElement('div');
-      shopItem.classList.add('shop__item');
-      shopBody.append(shopItem);
-      
-      const shopImg = document.createElement('div');
-      shopImg.classList.add('shop__image');
-      shopImg.style.backgroundImage = `url(${findCards[i].img})`;
-      shopItem.append(shopImg);
-    
-      const shopTitle = document.createElement('h3');
-      shopTitle.classList.add('shop__image_title');
-      shopTitle.innerHTML = findCards[i].name; // [i] номер объекта по порядку
-      shopItem.append(shopTitle);
-    
-      const price = document.createElement('p');
-      price.innerHTML = findCards[i].price; // [i] номер объекта по порядку
-      shopItem.append(price);
-     }
+  buyList.innerHTML = '';
+  const getBuyList = JSON.parse(localStorage.getItem('shopCart'));
 
+  showBuyProducts(getBuyList);
 
-    // if (findCards.length !== 0) { 
-    //   console.log(findCards);
-    //   showProduct(findCards);
-    // }
+// Функция которая отрисовывает блок tbody
+function showBuyProducts(arr) {
+  arr.forEach(el => {
+    let productItem = document.createElement('tr');
 
- };
+    productItem.innerHTML = `
+      <td>${el.name}</td>
+      <td class="table-image ${imagesObj[el.data]}"></td>
+      <td>${el.price}</td>
+      <td>${el.count}</td>
+      <td>${parseInt(el.price) * el.count}</td>
+      <td class="remove-item"></td>
+    `;
+    buyList.append(productItem);
+  });
+ }
+});
 
+//Поиск карточек.
  formBtn.addEventListener('click', searchCards);
  formInput.addEventListener('input', searchCards);
 
+
 const cards = [
   {
-    id: 1,
+    id: '01',
     name: 'ETHIOPIA COOFE 100гр.',
     img: 'img/s6-f1.png',
     price: '18 бел.р.',
     data: 'ETHIOPIA'
   },
   {
-    id: 2,
+    id: '02',
     name: 'ETHIOPIA COOFE 200гр.',
     img: 'img/s6-f1.png',
     price: '20 бел.р.',
     data: 'ETHIOPIA'
   },
   {
-    id: 3,
+    id: '03',
     name: 'ETHIOPIA COOFE 400гр.',
     img: 'img/s6-f1.png',
     price: '38 бел.р.',
     data: 'ETHIOPIA'
   },
   {
-    id: 4,
+    id: '04',
     name: 'ETHIOPIA COOFE 1 кг.',
     img: 'img/s6-f1.png',
     price: '76 бел.р.',
     data: 'ETHIOPIA'
   },
   {
-    id: 5,
+    id: '05',
     name: 'KENYA COOFE 100гр.',
     img: 'img/s6-f2.png',
     price: '20 бел.р.',
     data: 'KENYA'
   },
   {
-    id: 6,
+    id: '06',
     name: 'KENYA COOFE 200гр.',
     img: 'img/s6-f2.png',
     price: '22 бел.р.',
     data: 'KENYA'
   },
   {
-    id: 7,
+    id: '07',
     name: 'KENYA COOFE 400гр.',
     img: 'img/s6-f2.png',
     price: '40 бел.р.',
     data: 'KENYA'
   },
   {
-    id: 8,
+    id: '08',
     name: 'KENYA COOFE 1 кг.',
     img: 'img/s6-f2.png',
     price: '80 бел.р.',
     data: 'KENYA'
   },
   {
-    id: 9,
+    id: '09',
     name: 'QUATEMALA COOFE 100гр.',
     img: 'img/s6-f3.png',
     price: '26 бел.р.',
     data: 'QUATEMALA'
   },
   {
-    id: 10,
+    id: '10',
     name: 'QUATEMALA COOFE 200гр.',
     img: 'img/s6-f3.png',
     price: '28 бел.р.',
     data: 'QUATEMALA'
   },
   {
-    id: 11,
+    id: '11',
     name: 'QUATEMALA COOFE 400гр.',
     img: 'img/s6-f3.png',
     price: '46 бел.р.',
     data: 'QUATEMALA'
   },
   {
-    id: 12,
+    id: '12',
     name: 'QUATEMALA COOFE 1 кг.',
     img: 'img/s6-f3.png',
     price: '92 бел.р.',
     data: 'QUATEMALA'
   },
   {
-    id: 13,
+    id: '13',
     name: 'COLUMBIA COOFE 100гр.',
     img: 'img/s6-f4.png',
     price: '30 бел.р.',
     data: 'COLUMBIA'
   },
   {
-    id: 14,
+    id: '14',
     name: 'COLUMBIA COOFE 200гр.',
     img: 'img/s6-f4.png',
     price: '32 бел.р.',
     data: 'COLUMBIA'
   },
   {
-    id: 15,
+    id: '15',
     name: 'COLUMBIA COOFE 400гр.',
     img: 'img/s6-f4.png',
     price: '50 бел.р.',
     data: 'COLUMBIA'
   },
   {
-    id: 16,
+    id: '16',
     name: 'COLUMBIA COOFE 1 кг.',
     img: 'img/s6-f4.png',
     price: '100 бел.р.',
     data: 'COLUMBIA'
   },
 ];
+
+
+localStorage.setItem('cards', JSON.stringify(cards));
+
+// записать данные в локал сторэд ('cards' - ключ / cards - массив ) 
+// JSON.stringify - метод преобразования информации в строки для передачи в локал сторэдж
--- shopCard.js
+++ shopCard.js
@@ -3,19 +3,67 @@
 function shopCart() {
   const shopBody = document.querySelector('.shop__body');
 
-  shopBody.addEventListener('click', (e)=> {
+
+  const paintShopCart = () => {
+    const shopCart = JSON.parse(localStorage.getItem('shopCart')) || [];
+  };
+
+  const addCardToShopCart = (clickedCardId) => {
+    // ShopCart - корзина (массив)
+    // console.log(clickedCardId);
+    const cardsLS = JSON.parse(localStorage.getItem('cards'));
+    const card = cardsLS.find(el => el.id === clickedCardId);
+
+    const shopCart = JSON.parse(localStorage.getItem('shopCart')) || [];
+      // Есть ли в shopCart элемент с id равный card id 
+  // Метод some возвращает первый true
+  if (shopCart.some(el => el.id === card.id)) {
+    shopCart.map(el => {
+      if (el.id === card.id) {
+          el.count++; /* (el.count = el.count + 1) */
+      }
+    });   
+  } else {
+      card.count = 1;
+      shopCart.push(card);
+  }
+  localStorage.setItem('shopCart', JSON.stringify(shopCart));
+  };
+
+
+
+  shopBody.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     let clickedCard = e.target;
-    if(clickedCard.closest('shop__link')) {
-      console.log(clickedCard.dataset.id);
-      console.log(clickedCard.closest('.shop__link'));
-            // метол closest поднимается вверх по объектам и возвращает объект с указанным селектором
+    // console.log(e.target.closest('.shop__item').dataset.type);
+    // console.log((clickedCard.dataset.id));
+    if (e.target.closest('.shop__item')?.dataset.type) {
+      const newArr = [];
+      const type = e.target.closest('.shop__item').dataset.type;
+      //  console.log(type);
+      cards.forEach(el => {
+        if (el.data === type) {
+          newArr.push(el);
+        }
+      });
+      paintCards(newArr);
+    }
+
+    if (clickedCard.closest('.shop__link')) {
+      if (e.target.dataset.id !== undefined) {
+        const clickedCardId = e.target.dataset.id;
+
+        addCardToShopCart(clickedCardId);
+      }
+      // const arr = JSON.parse(localStorage.getItem('cards'));
+      // console.log(arr);
     }
   });
 
-      // const shopItem = document.querySelectorAll('shop__item');
-    // shopItem.addEventListener('click', ()=> {
-    //     console.log()
-    // });
-} shopCart();
+}
+
+shopCart();
+
+// console.log(clickedCard.closest('.shop__link'));
+// метол closest поднимается вверх по объектам и возвращает объект с указанным селектором
